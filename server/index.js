b48447858adfa5219703adc576deedcd4c2e2e39
Oscar Rodriguez <00206019@uca.edu.sv>
2021-11-17 15:00:15 -0500

    Modify: change modules to models

--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const ProductModel=require('./mode')
+
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost/pizzeria', { userNewUrlPaser: true, });
+
+app.get('/', (req,res)=>{
+
+})
 
 app.listen(3000,()=>{
     console.log("Running on port 3000");
